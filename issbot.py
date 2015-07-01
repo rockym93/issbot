49c149638a25bf1c1fe8da2a4e316af243926ab4
Rockwell McGellin <rockwell.m@gmail.com>
2015-07-01 12:37:57 +0800

    Basic offsetting.

--- issbot.py
+++ issbot.py
@@ -4,7 +4,18 @@ import feedparser
 import urllib.request
 import json
 
-with urllib.request.urlopen('https://api.telegram.org/bot122232540:AAHDFXIuQOzCaO0XGlBAGNdlcy5rLDYQ0O8/getUpdates') as f:
-	data = json.loads(f.read().decode('utf-8'))
+offset = '0'
+url = 'https://api.telegram.org/'
+botkey = 'bot122232540:AAHDFXIuQOzCaO0XGlBAGNdlcy5rLDYQ0O8'
 
-print(data)
+while True:
+	with urllib.request.urlopen(url + botkey + '/getUpdates?offset=' + offset) as f:
+		data = json.loads(f.read().decode('utf-8'))
+	
+	for update in data['result']:
+		offset = str(update['update_id'])
+		print(offset)
+		print(update['message']['text'])
+		
+
+		
