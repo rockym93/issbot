46423316354f27c4b2969c9375cc5df75280783d
Rockwell McGellin <rockwell.m@gmail.com>
2015-07-01 12:48:25 +0800

    Even smarter offsetting.

--- issbot.py
+++ issbot.py
@@ -4,18 +4,20 @@ import feedparser
 import urllib.request
 import json
 
-offset = '0'
 url = 'https://api.telegram.org/'
 botkey = 'bot122232540:AAHDFXIuQOzCaO0XGlBAGNdlcy5rLDYQ0O8'
+offset = 0
+timeout = 10
 
 while True:
-	with urllib.request.urlopen(url + botkey + '/getUpdates?offset=' + str(offset)) as f:
+	with urllib.request.urlopen(url + botkey + '/getUpdates?offset=' + str(offset) + '&timeout=' + str(timeout)) as f:
 		data = json.loads(f.read().decode('utf-8'))
-	
-	for update in data['result']:
-		print(update['message']['text'])
-	offset = data['result'][-1]['update_id']
-	print(offset)
+	if data['result']:
+		for update in data['result']:
+			print(update['message']['text'])
+		offset = data['result'][-1]['update_id'] + 1
+	else:
+		print("Nothing new to report.")
 		
 
 		
