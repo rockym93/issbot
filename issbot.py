6a2ff5983ec839ab4dc76615115604692efac0cd
Rockwell McGellin <rockwell.m@gmail.com>
2015-07-01 12:01:28 +0800

    Decoding from JSON.

--- issbot.py
+++ issbot.py
@@ -2,6 +2,9 @@
 
 import feedparser
 import urllib.request
+import json
 
 with urllib.request.urlopen('https://api.telegram.org/bot122232540:AAHDFXIuQOzCaO0XGlBAGNdlcy5rLDYQ0O8/getUpdates') as f:
-	print(f.read())
+	data = json.loads(f.read().decode('utf-8'))
+
+print(data)
