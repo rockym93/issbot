9ede33fc67cfd9a67eaeb7408acaab37fdde9632
Rockwell McGellin <rockwell.m@gmail.com>
2015-07-01 14:04:42 +0800

    Responding to commands.

--- issbot.py
+++ issbot.py
@@ -9,16 +9,32 @@ url = 'https://api.telegram.org/'
 botkey = 'bot122232540:AAHDFXIuQOzCaO0XGlBAGNdlcy5rLDYQ0O8'
 offset = 0
 
+helptext = '''I'm a bot who can help you spot the International Space Station passing overhead.
+
+/help - this text.'''
+
 def getUpdates(offset=0,timeout=0):
 	with urllib.request.urlopen(url + botkey + '/getUpdates?offset=' + str(offset) + '&timeout=' + str(timeout)) as f:
 		data = json.loads(f.read().decode('utf-8'))
 	return(data['result'])
 
 def processUpdate(update):
-	sendMessage(update['message']['from']['id'], update['message']['text'])
+	from_id = update['message']['from']['id']
+	text = update['message']['text']
+	message_id = update['message']['message_id']
+	if text == "/help":
+		sendMessage(from_id, helptext, message_id)
 	
 def sendMessage(to, text, reply=False):
-	tosend = urllib.parse.urlencode({'chat_id':to, 'text':text}).encode('utf-8')
+	parameters = {
+	'chat_id':to,
+	'text':text
+	}
+	
+	if reply:
+		parameters['reply_to_message_id'] = reply
+	
+	tosend = urllib.parse.urlencode(parameters).encode('utf-8')
 	urllib.request.urlopen(url + botkey + '/sendMessage?', tosend)
 	
 while True:
