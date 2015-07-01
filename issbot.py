10c77aa6024013d455893ca3de95f888eb6e7d93
Rockwell McGellin <rockwell.m@gmail.com>
2015-07-01 13:05:57 +0800

    Wrapped update retrieval in a function.

--- issbot.py
+++ issbot.py
@@ -7,17 +7,20 @@ import json
 url = 'https://api.telegram.org/'
 botkey = 'bot122232540:AAHDFXIuQOzCaO0XGlBAGNdlcy5rLDYQ0O8'
 offset = 0
-timeout = 10
 
-while True:
+def getUpdates(offset=0,timeout=0):
 	with urllib.request.urlopen(url + botkey + '/getUpdates?offset=' + str(offset) + '&timeout=' + str(timeout)) as f:
 		data = json.loads(f.read().decode('utf-8'))
-	if data['result']:
-		for update in data['result']:
-			print(update['message']['text'])
-		offset = data['result'][-1]['update_id'] + 1
-	else:
-		print("Nothing new to report.")
+	return(data['result'])
+
+while True:
+	updates = getUpdates(offset, 10)
+	if updates:
+		offset = updates[-1]['update_id'] + 1
 		
+		for update in updates:
+			print(update['message']['text'])
+
+
 
 		
