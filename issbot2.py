4ca4d6814bb1f6a143fe3cf42b2a4c58cf11219d
Rockwell McGellin <rockwell.m@gmail.com>
2015-12-02 16:59:29 +0800

    v2 complete, ready for some testing.

--- issbot2.py
+++ issbot2.py
@@ -5,6 +5,7 @@ import lazybot as bot
 import urllib
 import json
 import sys
+import time
 
 data = json.load(sys.stdin)
 
@@ -13,13 +14,32 @@ with key.txt as f:
 
 def pass(latitude, longitude):
 '''generic function for grabbing pass data given latitude and longitude'''
-# Insert that here.
+	with urllib.request.urlopen('http://api.open-notify.org/iss-pass.json?lat=' + str(latitude) + '&lon=' + str(longitude)) as f:
+		issdata = json.loads(f.read().decode('utf-8'))
+	nextpass = "The next time the ISS will pass overhead will be "
+	nextpass += time.strftime('%d %B at %H:%M UTC', time.gmtime(issdata['response'][0]['risetime']))
+	nextpass += " for " + str(issdata['response'][0]['duration']) + " seconds."
+	return nextpass
 
 
 ### Command Definitions ###
 
+def help(message):
+	helptext = '''I can help you spot the International Space Station passing overhead.
+
+Send me a location to get pass predictions, or /now to see where the station is right now.'''
+	tosend = {
+	'chat_id': message['chat']['id'],
+	'reply_to_message': message['message_id'],
+	'text': helptext
+	}
+	bot.api('sendMessage', tosend)
+
 def now(message):
-	# Insert now-fetching function here
+	with urllib.request.urlopen('http://api.open-notify.org/iss-now.json') as f:
+		issdata = json.loads(f.read().decode('utf-8'))
+	latitude = issdata['iss_position']['latitude']
+	longitude = issdata['iss_position']['longitude']
 	tosend = {
 	'chat_id': message['chat']['id'],
 	'reply_to_message': message['message_id'],
@@ -30,26 +50,14 @@ def now(message):
 
 bot.commands['/now'] = now
 
-def here(message):
-	# Extract latitude and longitude from input
-	
-	tosend = {
-	'chat_id': message['chat']['id'],
-	'reply_to_message': message['message_id'],
-	'text': "The ISS will next pass over your location at " + pass(lat, long)
-	}
-	bot.api('sendMessage', tosend)
-
-bot.commands['/here'] = here
-
 def location(message):
-	lat = message['location']['latitude']
-	long = message['location']['longitude']
+	latitude = message['location']['latitude']
+	longitude = message['location']['longitude']
 	
 	tosend = {
 	'chat_id': message['chat']['id'],
 	'reply_to_message': message['message_id'],
-	'text': "The ISS will next pass over your location at " + pass(lat, long)
+	'text': "The ISS will next pass over your location at " + pass(latitude, longitude)
 	}
 	bot.api('sendMessage', tosend)
 
